f6da8b546d9b4cc8af27ba9b3b2c2e0e17e70c9b
Prince Jain <pj122333444@gmail.com>
2025-03-11 20:44:19 +0530

    backend

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const port = 4000;
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    // methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log("id", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user Disconnected", socket.id);
+  });
+
+  socket.on("join", (data) => {
+    socket.join(data.roomId);
+    socket.emit("user joined", { data });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+server.listen(port, () => {
+  console.log(`serevr is running on ${port}`);
+});
