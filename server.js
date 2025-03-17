5aed17020d0f8527432a296d1edd319a14ddbb59
Prince Jain <pj122333444@gmail.com>
2025-03-18 05:10:47 +0530

    hint added

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = 4000;
 const server = createServer(app);
 const rooms = {};
 const points = {};
+const correctGuessers = {};
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -15,9 +16,13 @@ const io = new Server(server, {
   },
 });
 
-const wordSelect = 7;
+const wordSelect = 2;
 const wordGuess = 15;
-const maxRounds = 2;
+const maxRounds = 1;
+const hintTime = 10;
+
+const activeTimers = {};
+
 io.on("connection", (socket) => {
   console.log("user connected");
   console.log("id", socket.id);
@@ -31,6 +36,8 @@ io.on("connection", (socket) => {
         gamestarted: false,
         currentDrawerIndex: 0,
         currentRound: 1,
+        currentWord: "",
+        hintRevealed: false,
       };
     }
 
@@ -52,6 +59,12 @@ io.on("connection", (socket) => {
       );
       if (rooms[roomId].users.length === 0) {
         delete rooms[roomId];
+        delete correctGuessers[roomId];
+        if (activeTimers[roomId]) {
+          clearInterval(activeTimers[roomId].selectionTimer);
+          clearInterval(activeTimers[roomId].guessingTimer);
+          delete activeTimers[roomId];
+        }
       } else {
         io.to(roomId).emit("user-list", {
           users: rooms[roomId].users,
@@ -62,14 +75,20 @@ io.on("connection", (socket) => {
       }
     });
   });
+
   socket.on("start-game", (roomId) => {
     rooms[roomId].gamestarted = true;
-
     io.to(roomId).emit("game started");
-    wordSelectiontTime(roomId);
+    wordSelectionTime(roomId);
   });
 
-  function wordSelectiontTime(roomId) {
+  socket.on("reset-game", (roomId) => {
+    if (points[roomId]) delete points[roomId];
+    if (correctGuessers[roomId]) delete correctGuessers[roomId];
+    io.to(roomId).emit("scores-reset");
+  });
+
+  function wordSelectionTime(roomId) {
     const room = rooms[roomId];
     if (room) {
       const currentDrawer = room.users[room.currentDrawerIndex];
@@ -82,30 +101,132 @@ io.on("connection", (socket) => {
         drawerId: currentDrawer.id,
         name: currentDrawer.name,
       });
+
+      correctGuessers[roomId] = [];
+      room.hintRevealed = false;
+
       let timeLeft = wordSelect;
 
-      const interval = setInterval(() => {
+      if (activeTimers[roomId] && activeTimers[roomId].selectionTimer) {
+        clearInterval(activeTimers[roomId].selectionTimer);
+      }
+
+      if (!activeTimers[roomId]) activeTimers[roomId] = {};
+
+      activeTimers[roomId].selectionTimer = setInterval(() => {
         timeLeft--;
         io.to(roomId).emit("word-selection-time", timeLeft);
         if (timeLeft === 0) {
-          clearInterval(interval);
+          clearInterval(activeTimers[roomId].selectionTimer);
           wordGuessingTime(roomId);
         }
-      }, [1000]);
+      }, 1000);
     }
   }
 
   function wordGuessingTime(roomId) {
-    let timeLeft = wordGuess;
+    const totalTime = wordGuess;
+    let timeLeft = totalTime;
 
-    const interval = setInterval(() => {
+    if (activeTimers[roomId] && activeTimers[roomId].guessingTimer) {
+      clearInterval(activeTimers[roomId].guessingTimer);
+    }
+
+    io.to(roomId).emit("message", {
+      message: "Guessing time has started!",
+      name: "System",
+    });
+
+    rooms[roomId].totalGuessingTime = totalTime;
+
+    activeTimers[roomId].guessingTimer = setInterval(() => {
       timeLeft--;
       io.to(roomId).emit("word-guessing-time", timeLeft);
+
+      if (timeLeft === hintTime && !rooms[roomId].hintRevealed) {
+        rooms[roomId].hintRevealed = true;
+        revealHint(roomId);
+      }
+
       if (timeLeft === 0) {
-        clearInterval(interval);
+        clearInterval(activeTimers[roomId].guessingTimer);
+
+        const room = rooms[roomId];
+        const currentDrawer = room.users[room.currentDrawerIndex];
+        const drawerName = currentDrawer.name;
+
+        if (!points[roomId]) points[roomId] = {};
+        if (!points[roomId][drawerName]) points[roomId][drawerName] = 0;
+
+        const guessers = correctGuessers[roomId] || [];
+        const correctGuessCount = guessers.length;
+        const totalPossibleGuessers = room.users.length - 1;
+
+        if (correctGuessCount > 0) {
+          const percentCorrect = correctGuessCount / totalPossibleGuessers;
+          const drawerBonus = Math.round(10 * percentCorrect);
+          points[roomId][drawerName] += drawerBonus;
+
+          io.to(roomId).emit("message", {
+            message: `${drawerName} earned ${drawerBonus} points for their drawing!`,
+            name: "System",
+          });
+
+          io.to(roomId).emit("scores-update", points[roomId]);
+        }
+
+        if (correctGuessCount < totalPossibleGuessers) {
+          io.to(roomId).emit("message", {
+            message: `The word was: ${room.currentWord}`,
+            name: "System",
+          });
+        }
+
         proceedToNextTurn(roomId);
       }
-    }, [1000]);
+    }, 1000);
+  }
+
+  function revealHint(roomId) {
+    const room = rooms[roomId];
+    if (!room || !room.currentWord) return;
+
+    const word = room.currentWord;
+
+    let maskedWord = word
+      .split("")
+      .map(() => "_")
+      .join("");
+
+    let positions = [];
+    while (positions.length < 2 && positions.length < word.length) {
+      const randomPos = Math.floor(Math.random() * word.length);
+      if (!positions.includes(randomPos)) {
+        positions.push(randomPos);
+      }
+    }
+
+    positions.forEach((pos) => {
+      maskedWord =
+        maskedWord.substring(0, pos) +
+        word[pos] +
+        maskedWord.substring(pos + 1);
+    });
+
+    const currentDrawer = room.users[room.currentDrawerIndex];
+    room.users.forEach((user) => {
+      if (user.id !== currentDrawer.id) {
+        io.to(user.id).emit("word-hint", {
+          hint: maskedWord,
+          message: "HINT: Two letters revealed!",
+        });
+      }
+    });
+
+    io.to(roomId).emit("message", {
+      message: "Time is running out! A hint has been revealed to guessers.",
+      name: "System",
+    });
   }
 
   function proceedToNextTurn(roomId) {
@@ -121,29 +242,114 @@ io.on("connection", (socket) => {
           room.gamestarted = false;
           room.currentDrawerIndex = 0;
           room.currentRound = 1;
-          points[roomId] = {};
           io.to(roomId).emit("game-ended");
         } else {
           io.to(roomId).emit("clear-canvas");
-          wordSelectiontTime(roomId);
+          io.to(roomId).emit("message", {
+            message: `Round ${room.currentRound} is starting...`,
+            name: "System",
+          });
+          wordSelectionTime(roomId);
         }
       } else {
         io.to(roomId).emit("clear-canvas");
-        wordSelectiontTime(roomId);
+        wordSelectionTime(roomId);
       }
     }
   }
 
   socket.on("correct-guess", ({ name, roomId }) => {
+    const room = rooms[roomId];
+
+    if (correctGuessers[roomId] && correctGuessers[roomId].includes(name)) {
+      return;
+    }
+
+    if (!room || !room.gamestarted) return;
+
+    const currentDrawer = room.users[room.currentDrawerIndex];
+    if (!currentDrawer || currentDrawer.name === name) return;
+
     if (!points[roomId]) points[roomId] = {};
     if (!points[roomId][name]) points[roomId][name] = 0;
 
-    points[roomId][name] += 10;
+    const timeRemaining = activeTimers[roomId]
+      ? parseInt(
+          room.totalGuessingTime -
+            (room.totalGuessingTime -
+              activeTimers[roomId].guessingTimer._idleTimeout / 1000)
+        )
+      : 0;
+    const totalTime = room.totalGuessingTime || wordGuess;
+
+    const timeRatio = timeRemaining / totalTime;
+    const timeBonus = Math.round(10 * timeRatio);
+    const totalPoints = 10 + timeBonus;
+
+    const finalPoints = room.hintRevealed
+      ? Math.round(totalPoints * 0.8)
+      : totalPoints;
+
+    points[roomId][name] += finalPoints;
+
+    if (!correctGuessers[roomId]) correctGuessers[roomId] = [];
+    correctGuessers[roomId].push(name);
+
+    const drawerName = currentDrawer.name;
+    if (!points[roomId][drawerName]) points[roomId][drawerName] = 0;
+    points[roomId][drawerName] += 5;
+
+    io.to(socket.id).emit("message", {
+      message: room.hintRevealed
+        ? `You guessed correctly! You earned ${finalPoints} points (${timeBonus} time bonus, but reduced because you saw a hint)`
+        : `You guessed correctly! You earned ${finalPoints} points (${timeBonus} time bonus)`,
+      name: "System",
+    });
+
+    io.to(roomId).emit("message", {
+      message: `${name} guessed the word correctly!`,
+      name: "System",
+    });
 
     io.to(roomId).emit("scores-update", points[roomId]);
+
+    const totalPossibleGuessers = room.users.length - 1;
+    if (correctGuessers[roomId].length >= totalPossibleGuessers) {
+      if (activeTimers[roomId] && activeTimers[roomId].guessingTimer) {
+        clearInterval(activeTimers[roomId].guessingTimer);
+      }
+
+      points[roomId][drawerName] += 10;
+      io.to(roomId).emit("message", {
+        message: `Everyone guessed the word! ${drawerName} gets a 10 point bonus!`,
+        name: "System",
+      });
+      io.to(roomId).emit("scores-update", points[roomId]);
+
+      setTimeout(() => {
+        proceedToNextTurn(roomId);
+      }, 3000);
+    }
+  });
+
+  socket.on("force-end-timer", (roomId) => {
+    const room = rooms[roomId];
+    if (room) {
+      if (activeTimers[roomId] && activeTimers[roomId].selectionTimer) {
+        clearInterval(activeTimers[roomId].selectionTimer);
+      }
+
+      io.to(roomId).emit("word-selection-time", 0);
+
+      wordGuessingTime(roomId);
+    }
   });
 
   socket.on("word-selected", ({ roomId, word }) => {
+    if (rooms[roomId]) {
+      rooms[roomId].currentWord = word;
+    }
+
     io.to(roomId).emit("word-to-guess", word);
   });
 
@@ -152,6 +358,7 @@ io.on("connection", (socket) => {
     console.log("name", name);
     io.to(roomId).emit("message", { message, name });
   });
+
   socket.on(
     "drawing",
     ({ roomId, prevX, prevY, x, y, brushColor, brushSize }) => {
@@ -171,5 +378,5 @@ app.get("/", (req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`serevr is running on ${port}`);
+  console.log(`server is running on ${port}`);
 });
