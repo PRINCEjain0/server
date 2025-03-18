461b5f095074854fb02d8f625898294fd2082108
Prince Jain <pj122333444@gmail.com>
2025-03-18 05:31:45 +0530

    front url added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const points = {};
 const correctGuessers = {};
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://sketchrace.vercel.app/",
     methods: ["GET", "POST"],
     credentials: true,
   },
